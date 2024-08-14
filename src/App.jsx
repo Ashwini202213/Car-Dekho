1090b71d6900d25d2e789dff94c463fa9de6bea9
suraj vaidya <surajvaidya196@gmail.com>
2024-08-14 19:55:10 +0530

    initial modify app

--- src/App.jsx
+++ src/App.jsx
@@ -8,50 +8,12 @@ const App = () => {
     <div className="App">
           
         
-<<<<<<< HEAD
-      </header>
-      <div className="car-section">
-      <h3>The Most Searched Cars </h3>
-        <div className="car-list">
-          {newCarData.map(car => (
-            <CarCard
-              key={car.id}
-              name={car.name}
-              price={car.price}
-              image={car.image}
-              offers={car.offers}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="car-section">
-        <h1>Welcome</h1>
-        <h2>Recommended Used Cars For You</h2>
-        <h3>test code</h3>
-        <div className="car-list">
-          {usedCarData.map(car => (
-            <CarCard
-              key={car.id}
-              name={car.name}
-              price={car.price}
-              image={car.image}
-              details={car.details}
-              button={car.button}
-            />
-          ))}
-        </div>
-      </div>
-      {/* <PopularBrands /> */}
-      {/* <CarRecommender/> */}
-    </div>
-=======
          <HomePage/>
          
 
          <ExploreNewCars/>
          
        </div>
->>>>>>> 98f0230afc06225015657e295c7cd1063db5717d
 
   );
 };
